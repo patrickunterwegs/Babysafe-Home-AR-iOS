f3ea4c2f16dca41726ae721b4880e8c5f567113b
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2022-03-28 20:59:35 +0200

    Format updates Global settings updates (App name)

--- Babysafe Home AR iOS SwiftUI/Models/BabyDanger.swift
+++ Babysafe Home AR iOS SwiftUI/Models/BabyDanger.swift
@@ -21,8 +21,26 @@ struct BabyDanger: Identifiable {
 extension BabyDanger {
     static let allBabyDangers: [BabyDanger] =
     [
-        BabyDanger(id: "POWER", objectIds: [255], title: String("danger_power_title"), description: String("danger_power_text"), linkGlobal: URL.init(string: "https://www.orf.at"), linkAmazonDE: URL.init(string: "https://www.amazon.at")),
-        BabyDanger(id: "TOILETWATER", objectIds: [125, 457, 459], title: String("danger_toiletwater_title"), description: String("danger_toiletwater_text"), linkGlobal: URL.init(string: "https://www.orf.at"), linkAmazonDE: URL.init(string: "https://www.amazon.at")),
-        BabyDanger(id: "FURNITURE_TIPOVER", objectIds: [217, 364, 379, 492], title: String("danger_furniture_tipover_title"), description: String("danger_furniture_tipover_text"), linkGlobal: URL.init(string: "https://www.orf.at"), linkAmazonDE: URL.init(string: "https://www.amazon.at"))
+        BabyDanger(
+            id: "POWER",
+            objectIds: [255],
+            title: String("danger_power_title"),
+            description: String("danger_power_text"),
+            linkGlobal: URL.init(string: "https://www.orf.at"),
+            linkAmazonDE: URL.init(string: "https://www.amazon.at")),
+        BabyDanger(
+            id: "TOILETWATER",
+            objectIds: [125, 457, 459],
+            title: String("danger_toiletwater_title"),
+            description: String("danger_toiletwater_text"),
+            linkGlobal: URL.init(string: "https://www.orf.at"),
+            linkAmazonDE: URL.init(string: "https://www.amazon.at")),
+        BabyDanger(
+            id: "FURNITURE_TIPOVER",
+            objectIds: [217, 364, 379, 492],
+            title: String("danger_furniture_tipover_title"),
+            description: String("danger_furniture_tipover_text"),
+            linkGlobal: URL.init(string: "https://www.orf.at"),
+            linkAmazonDE: URL.init(string: "https://www.amazon.at"))
     ]
 }
