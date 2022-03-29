8bd3f8433f00f5d12e1c39b457c4184a0eca6c1b
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2022-03-29 22:00:15 +0200

    further improvements with mobileNet V2

--- Babysafe Home AR iOS SwiftUI/CameraView.swift
+++ Babysafe Home AR iOS SwiftUI/CameraView.swift
@@ -7,10 +7,18 @@
 
 import SwiftUI
 import AVFoundation
+import Vision
 
 struct CameraView: View {
     
+    @State var objectIdentifier: String = "test"
+    @State var objectConfidence: VNConfidence = 99
+    
+    
     @StateObject var camera = CameraModel()
+    @State private var isChecklistSheetPresented: Bool = true
+    
+    
     
     var body: some View {
         ZStack {
@@ -18,100 +26,147 @@ struct CameraView: View {
             //Color.black.ignoresSafeArea(.all, edges: .all)
             //Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).foregroundColor(.white)
         }
+        .sheet(isPresented: $isChecklistSheetPresented, onDismiss: {
+            print("Dismiss")
+            self.isChecklistSheetPresented = false
+            
+        }, content: {
+            VStack {
+                
+            }
+        }).padding(8)
         .onAppear(perform: {
             camera.checkPermission()
         })
+        .onDisappear(perform: {
+            camera.session.stopRunning()
+        })
+        
+        
         
     }
-}
-
-struct CameraView_Previews: PreviewProvider {
-    static var previews: some View {
-        CameraView()
+    
+    
+    struct CameraView_Previews: PreviewProvider {
+        static var previews: some View {
+            CameraView()
+        }
     }
-}
-
-
-
-// camera model
-class CameraModel: ObservableObject {
     
-    @Published var session = AVCaptureSession()
-    @Published var alert = false
-    @Published var preview: AVCaptureVideoPreviewLayer!
     
-    func checkPermission() {
+    
+    // https://www.youtube.com/watch?v=8hvaniprctk
+    
+    // camera model
+    class CameraModel: NSObject, ObservableObject, AVCaptureVideoDataOutputSampleBufferDelegate {
         
-        switch AVCaptureDevice.authorizationStatus(for: .video) {
+       
+        @Published var session = AVCaptureSession()
+        @Published var alert = false
+        @Published var preview: AVCaptureVideoPreviewLayer!
+        @Published var output = AVCaptureVideoDataOutput()
+        
+        let model = MobileNetV2().model
+        
+        func checkPermission() {
             
-        case .notDetermined:
-            //requesting permission
-            AVCaptureDevice.requestAccess(for: .video) { (status) in
+            switch AVCaptureDevice.authorizationStatus(for: .video) {
                 
-                if status {
-                    self.setUp()
+            case .notDetermined:
+                //requesting permission
+                AVCaptureDevice.requestAccess(for: .video) { (status) in
+                    
+                    if status {
+                        self.setUp()
+                    }
+                }
+            case .denied:
+                self.alert.toggle()
+                return
+            case .authorized:
+                setUp()
+                return
+            default:
+                return
+            }
+        }
+        
+        func setUp() {
+            // setting up camera
+            
+            do {
+                // setting configs
+                self.session.beginConfiguration()
+                
+                let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+                let input = try AVCaptureDeviceInput(device: device!)
+                
+                if self.session.canAddInput(input) {
+                    self.session.addInput(input)
+                }
+                
+                // same for output
+                if self.session.canAddOutput(self.output) {
+                    output.setSampleBufferDelegate(self, queue: DispatchQueue(label: "CameraView"))
+                    self.session.addOutput(self.output)
+                }
+                
+                self.session.commitConfiguration()
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+        }
+        
+        func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+            
+            guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+            guard let model = try? VNCoreMLModel(for: self.model) else { return }
+            
+            let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
+                
+                guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
+                //guard let firstObservation = results.first else { return }
+                
+                DispatchQueue.main.async {
+                    
+                    results.forEach{  result in
+                        if result.confidence > 0.7 {
+                            print("Identifier \(result.identifier)")
+                            print("Confidence \(result.confidence * 100)")
+                            
+                            //objectIdentifier = result.identifier
+                        }
+                    }
                 }
             }
-        case .denied:
-            self.alert.toggle()
-            return
-        case .authorized:
-            setUp()
-            return
-        default:
-            return
+            
+            try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
         }
     }
     
-    func setUp() {
-        // setting up camera
+    
+    // setting view for preview view to use in UI
+    
+    struct CameraPreview: UIViewRepresentable {
         
-        do {
-            // setting configs
-            self.session.beginConfiguration()
+        @ObservedObject var camera: CameraModel
+        
+        func makeUIView(context: Context) -> UIView {
+            let view = UIView(frame: UIScreen.main.bounds)
+            camera.preview = AVCaptureVideoPreviewLayer(session: camera.session)
+            camera.preview.frame = view.frame
             
-            let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
-            let input = try AVCaptureDeviceInput(device: device!)
+            camera.preview.videoGravity = .resizeAspectFill
+            view.layer.addSublayer(camera.preview)
             
-            if self.session.canAddInput(input) {
-                self.session.addInput(input)
-            }
+            // start session
+            camera.session.startRunning()
             
-            // not handling output, if necessary check video again!!!
-            // https://www.youtube.com/watch?v=8hvaniprctk
-            
-            self.session.commitConfiguration()
-        }
-        catch {
-            print(error.localizedDescription)
+            return view
         }
         
+        func updateUIView(_ uiView: UIView, context: Context) {   }
+        
     }
-    
-}
-
-
-// setting view for preview
-
-struct CameraPreview: UIViewRepresentable {
-
-    
-    @ObservedObject var camera: CameraModel
-    
-    func makeUIView(context: Context) -> UIView {
-        let view = UIView(frame: UIScreen.main.bounds)
-        camera.preview = AVCaptureVideoPreviewLayer(session: camera.session)
-        camera.preview.frame = view.frame
-        
-        camera.preview.videoGravity = .resizeAspectFill
-        view.layer.addSublayer(camera.preview)
-        
-        // start session
-        camera.session.startRunning()
-        
-        return view
-    }
-    
-    func updateUIView(_ uiView: UIView, context: Context) {   }
-    
 }
