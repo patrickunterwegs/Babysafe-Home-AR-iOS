003c0c4c28ea960119b31ae1566499ec0120436c
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2022-03-30 20:02:12 +0200

    Basic integration von Googles MLKit

--- Babysafe Home AR iOS SwiftUI/CameraView.swift
+++ Babysafe Home AR iOS SwiftUI/CameraView.swift
@@ -8,14 +8,15 @@
 import SwiftUI
 import AVFoundation
 import Vision
+import CoreVideo
+import MLImage
+import MLKit
 
 struct CameraView: View {
     
-
     
     @StateObject var camera = CameraModel()
     @State private var isChecklistSheetPresented: Bool = false
-    
     
     
     var body: some View {
@@ -31,7 +32,6 @@ struct CameraView: View {
                     }, content: {
                         VStack {
                             Text(String(camera.objectIdentifier))
-                            Text(String(camera.objectConfidence))
                         }
                     }).padding(8)
             }
@@ -42,13 +42,12 @@ struct CameraView: View {
             })
             .onDisappear(perform: {
                 camera.session.stopRunning()
-        })
+            })
             
             Spacer()
             GroupBox {
                 HStack {
                     Text(String(camera.objectIdentifier))
-                    Text(String(camera.objectConfidence))
                 }
             }
         }
@@ -71,21 +70,14 @@ struct CameraView: View {
     // camera model
     class CameraModel: NSObject, ObservableObject, AVCaptureVideoDataOutputSampleBufferDelegate {
         
-       
+        
         @Published var session = AVCaptureSession()
         @Published var alert = false
         @Published var preview: AVCaptureVideoPreviewLayer!
         @Published var output = AVCaptureVideoDataOutput()
         
         @Published var objectIdentifier: String = ""
-        @Published var objectConfidence: VNConfidence = 0
-                
         
-        let model = MobileNetV2().model
-        //let model = SqueezeNet().model
-        //let model = YOLOv3Tiny().model
-        //let model = Resnet50Int8LUT().model
-        //let model = Inceptionv3().model
         
         func checkPermission() {
             
@@ -137,31 +129,85 @@ struct CameraView: View {
             }
         }
         
-        func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-            
-            guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
-            guard let model = try? VNCoreMLModel(for: self.model) else { return }
-            
-            let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
-                
-                guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
-                //guard let firstObservation = results.first else { return }
-                
-                DispatchQueue.main.async {
-                    
-                    results.forEach{  result in
-                        if result.confidence > 0.6 {
-                            print("Identifier \(result.identifier)")
-                            print("Confidence \(result.confidence * 100)")
-                            
-                            self.objectIdentifier = result.identifier
-                            self.objectConfidence = result.confidence
-                        }
-                    }
-                }
+        
+        func captureOutput(
+            _ output: AVCaptureOutput,
+            didOutput sampleBuffer: CMSampleBuffer,
+            from connection: AVCaptureConnection
+        ) {
+            guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+                print("Failed to get image buffer from sample buffer.")
+                return
             }
             
-            try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+            //lastFrame = sampleBuffer
+            let visionImage = VisionImage(buffer: sampleBuffer)
+            //let orientation = UIUtilities.imageOrientation(fromDevicePosition: .back)
+            //visionImage.orientation = orientation
+            
+            guard let inputImage = MLImage(sampleBuffer: sampleBuffer) else {
+                print("Failed to create MLImage from sample buffer.")
+                return
+            }
+            //inputImage.orientation = orientation
+            
+            let imageWidth = CGFloat(CVPixelBufferGetWidth(imageBuffer))
+            let imageHeight = CGFloat(CVPixelBufferGetHeight(imageBuffer))
+            
+            detectLabels(
+                in: visionImage, width: imageWidth, height: imageHeight)
+        }
+        
+        
+        func detectLabels(
+            in visionImage: VisionImage,
+            width: CGFloat,
+            height: CGFloat
+        ) {
+            
+            // load local model
+            var options: CommonImageLabelerOptions!
+            guard
+                let localModelFilePath = Bundle.main.path(
+                    forResource: Constant.localModelFile.name,
+                    ofType: Constant.localModelFile.type
+                )
+            else {
+                print("On-Device label detection failed because custom model was not found.")
+                return
+            }
+            
+            let localModel = LocalModel(path: localModelFilePath)
+            options = CustomImageLabelerOptions(localModel: localModel)
+            options.confidenceThreshold = NSNumber(floatLiteral: Constant.labelConfidenceThreshold)
+            
+            let onDeviceLabeler = ImageLabeler.imageLabeler(options: options)
+            let labels: [ImageLabel]
+            do {
+                labels = try onDeviceLabeler.results(in: visionImage)
+            } catch let error {
+                let errorString = error.localizedDescription
+                print("On-Device label detection failed with error: \(errorString)")
+                //self.updatePreviewOverlayViewWithLastFrame()
+                return
+            }
+            
+            let resultsText = labels.map { label -> String in
+                return "Label: \(label.text), Confidence: \(label.confidence), Index: \(label.index)"
+            }.joined(separator: "\n")
+            
+            //self.updatePreviewOverlayViewWithLastFrame()
+            
+            guard resultsText.count != 0 else { return }
+            
+            weak var weakSelf = self
+            DispatchQueue.main.sync {
+                
+                // Check labels and update UI here!!!
+                print(resultsText)
+                objectIdentifier = resultsText
+                
+            }
         }
     }
     
@@ -188,5 +234,15 @@ struct CameraView: View {
         
         func updateUIView(_ uiView: UIView, context: Context) {   }
         
+    }
+    
+    
+    
+    private enum Constant {
+      static let videoDataOutputQueueLabel = "com.google.mlkit.visiondetector.VideoDataOutputQueue"
+      static let sessionQueueLabel = "com.google.mlkit.visiondetector.SessionQueue"
+      static let localModelFile = (name: "lite-model_object_detection_mobile_object_labeler_v1_1", type: "tflite")
+      static let labelConfidenceThreshold = 0.8
+
     }
 }
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '15.0'
+
+target 'Babysafe Home AR iOS SwiftUI' do
+  # Comment the next line if you don't want to use dynamic frameworks
+use_frameworks!
+
+pod 'GoogleMLKit/ImageLabelingCustom', '2.6.0'
+pod 'GoogleMLKit/ObjectDetectionCustom', '2.6.0'
+
+
+  # Pods for Babysafe Home AR iOS SwiftUI
+
+  target 'Babysafe Home AR iOS SwiftUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Babysafe Home AR iOS SwiftUIUITests' do
+    # Pods for testing
+  end
+
+end
